1e11bc568b2d0e4a11519cb69413e646028ffc34
Santiago Ovalle <133941360+Sovalle93@users.noreply.github.com>
2024-04-18 18:25:37 -0400

    Update seeJobRoutes.js

--- api/config/routes/seeJobRoutes.js
+++ api/config/routes/seeJobRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
+import { decodeEmail } from "../../middlewares/validateEmail.js"
 import { lookJobsController } from "../../src/controllers/seeJobsController.js"
 
 const router = express.Router()
 
-router.get("/enterprise", lookJobsController)
+router.get("/enterprise", decodeEmail, lookJobsController)
 
 export default router
